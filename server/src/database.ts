7b372abe36b7e7f7ec68a92f6a5a63ca1aeb1dc8
Abdoul Hakim Billa <abdoulhakimbilla30@outlook.com>
2023-05-30 22:25:15 +0200

    ïnit server backend

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,26 +1,52 @@
-import * as mongodb from 'mongodb';
-import { Project } from './Project';
-
+import * as mongodb from "mongodb";
+import { Project } from "./Project";
 
 export const collections: {
-  projects?: mongodb.Collection<Project>
+  projects?: mongodb.Collection<Project>;
 } = {};
-
 
 // connect to the databas
 /**
  * Initializes the connection to the database
  * @param uri database uri used to connect to the databsse
  */
-async function connectToDatabase(uri:string) {
+async function connectToDatabase(uri: string) {
   //create mongodb client
   const client = new mongodb.MongoClient(uri);
   await client.connect();
 
   // connect to the database
   const db = client.db("kanban-board-db");
+  await applySchemaValidation(db);
   // TODO consider adding schema validation
   // assign retrived projects
   const projectCollection = db.collection<Project>("projects");
   collections.projects = projectCollection;
+}
+
+// update json validation so that the projects will always look the same
+async function applySchemaValidation(db: mongodb.db) {
+  const jsonSchema = {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["name"],
+      additionalProperties: false,
+      properties: {
+        _id: {},
+        name: {
+          bsonType: "string",
+          description: "'name' is required and is a string",
+        },
+      },
+    },
+  };
+  // Try applying the modification to the collection, if the collection doesn't exist, create it
+  await db.command({
+    collMod: "projects",
+    validator: jsonSchema
+  }).catch(async (error: mongodb.MongoServerError) => {
+    if (error.codeName === 'NamespaceNotFound') {
+      await db.createCollection("projects", {validator: jsonSchema})
+    }
+  })
 }
